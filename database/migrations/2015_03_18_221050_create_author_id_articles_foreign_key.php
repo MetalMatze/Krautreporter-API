5af9d983f5e50d0dd1c1d0405c70b1073a7e3c36
MetalMatze <me@metalmatze.de>
2015-03-18 23:36:01 +0100

    Create migration for author_id foreign key in articles table

---
+++ database/migrations/2015_03_18_221050_create_author_id_articles_foreign_key.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorIdArticlesForeignKey extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function($table) {
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function($table) {
+            $table->dropForeign('articles_author_id_foreign');
+        });
+    }
+
+}
