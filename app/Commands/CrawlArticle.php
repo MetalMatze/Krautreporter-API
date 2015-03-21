e226e7f30a5d9ccd6fc847ece01da87ad30c8662
MetalMatze <me@metalmatze.de>
2015-03-21 14:57:06 +0100

    Create CrawlArticle command

---
+++ app/Commands/CrawlArticle.php
@@ -0,0 +1,96 @@
+<?php namespace App\Commands;
+
+use App\Article;
+use App\Commands\Command;
+
+use Carbon\Carbon;
+use Goutte\Client;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+use Illuminate\Support\Facades\Log;
+
+class CrawlArticle extends Command implements SelfHandling, ShouldBeQueued {
+
+    use InteractsWithQueue, SerializesModels;
+    /**
+     * @var Article
+     */
+    private $article;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(Article $article)
+    {
+        //
+        $this->article = $article;
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = new Client();
+
+        $crawler = $client->request('GET', 'https://krautreporter.de' . $this->article->url);
+
+        $articleNode = $crawler->filter('main article.article.article--full')->eq(1);
+        $articleHeaderNode = $articleNode->filter('header.article__header');
+        $articleContentNode = $articleNode->filter('.article__content');
+
+        $articleDateText = $articleHeaderNode->filter('h2.meta')->text();
+
+        $this->article->headline = $articleHeaderNode->filter('h1.article__title')->text();
+        $this->article->date = Carbon::createFromFormat('d.m.Y', $articleDateText);
+
+        $articleImageNode = $articleHeaderNode->filter('.media__img img');
+
+        if($articleImageNode->count() > 0)
+        {
+            $this->article->image = $articleImageNode->attr('src');
+        }
+
+        $this->article->excerpt = trim($articleContentNode->filter('h2.gamma')->text());
+        $this->article->content = trim($articleContentNode->html());
+
+        $this->article->save();
+
+        $this->calculateNextCrawlDate();
+    }
+
+    private function calculateNextCrawlDate()
+    {
+        if(Carbon::now()->diffInMonths($this->article->date) > 0)
+        {
+            $nextCrawl = Carbon::now()->addDays(3);
+        }
+        elseif(Carbon::now()->diffInWeeks($this->article->date) > 0)
+        {
+            $nextCrawl = Carbon::now()->addDay();
+        }
+        elseif(Carbon::now()->diffInDays($this->article->date) > 0)
+        {
+            $nextCrawl = Carbon::now()->addHours(6);
+        }
+        else
+        {
+            $nextCrawl = Carbon::now()->addHours(2);
+        }
+
+        Log::info(sprintf('Article %s is crawled again in %s', $this->article->url, $nextCrawl->diffForHumans()));
+
+        $crawl = $this->article->crawl;
+        $crawl->next_crawl = $nextCrawl;
+
+        $this->article->crawl()->save($crawl);
+
+    }
+
+}
