eab2c8e8b6d8bcd87dc3164832b7587823fe77e2
MetalMatze <me@metalmatze.de>
2015-03-29 17:33:06 +0200

    Drop image field from authors table

---
+++ database/migrations/2015_03_29_155806_drop_image_from_authors.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropImageFromAuthors extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('authors', function($table) {
+            $table->dropColumn('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('authors', function($table) {
+            $table->string('image')->nullable();
+        });
+    }
+
+}
