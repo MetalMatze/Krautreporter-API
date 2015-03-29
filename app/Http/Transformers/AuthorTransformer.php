0a375a8e3401b4ba2258158caa6448ff6d92e28f
MetalMatze <me@metalmatze.de>
2015-03-29 17:34:38 +0200

    Add image include as default to AuthorTransformer

--- app/Http/Transformers/AuthorTransformer.php
+++ app/Http/Transformers/AuthorTransformer.php
@@ -6,6 +6,14 @@ use League\Fractal\TransformerAbstract;
 
 class AuthorTransformer extends TransformerAbstract {
 
+    protected $availableIncludes = [
+        'images'
+    ];
+
+    protected $defaultIncludes = [
+        'images'
+    ];
+
     public function transform(Author $author)
     {
         return [
@@ -13,12 +21,18 @@ class AuthorTransformer extends TransformerAbstract {
             'name' => $author->name,
             'title' => $author->title,
             'url' => $author->url,
-            'image' => $author->image,
             'biography' => $author->biography,
             'socialmedia' => $author->socialmedia,
             'created_at' => $author->created_at->format(\DateTime::ISO8601),
             'updated_at' => $author->updated_at->format(\DateTime::ISO8601),
         ];
+    }
+
+    public function includeImages(Author $author)
+    {
+        $images = $author->images;
+
+        return $this->collection($images, new ImageTransformer());
     }
 
 }
