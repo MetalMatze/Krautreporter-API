311fc604bd52af8eb02c785280126a7607670424
MetalMatze <me@metalmatze.de>
2015-04-01 22:51:57 +0200

    Create add_preview_field_to_articles migration

---
+++ database/migrations/2015_04_01_221755_add_preview_field_to_articles.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPreviewFieldToArticles extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function($table) {
+            $table->boolean('preview')->default(false)->after('morgenpost');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function($table) {
+            $table->dropColumn('preview');
+        });
+    }
+
+}
