4008c4e84d4699e79575a3f79564e17eef1ce9fe
MetalMatze <me@metalmatze.de>
2015-05-03 22:31:23 +0200

    Add PHPDocs to Repository interface

--- app/Krautreporter/Repository.php
+++ app/Krautreporter/Repository.php
@@ -2,14 +2,33 @@
 namespace App\Krautreporter;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 interface Repository
 {
+    /**
+     * @param array $fields
+     * @return mixed
+     */
     public function all(array $fields = []);
 
+    /**
+     * @param $id
+     * @return Model
+     * @throws ModelNotFoundException
+     */
     public function find($id);
 
+    /**
+     * @param Model $model
+     * @return bool
+     */
     public function save(Model $model);
 
+    /**
+     * @param Model $model
+     * @return mixed
+     * @throws \Exception
+     */
     public function delete(Model $model);
 }
