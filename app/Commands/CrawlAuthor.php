037759beba179b55e2f8ae61723f6977bff9f9a5
Matthias Loibl <me@metalmatze.de>
2015-08-06 12:26:12 +0200

    Crawl author only if got 200 response

--- app/Commands/CrawlAuthor.php
+++ app/Commands/CrawlAuthor.php
@@ -40,45 +40,46 @@ class CrawlAuthor extends Command implements SelfHandling, ShouldBeQueued
 
         $crawler = $client->request('GET', 'https://krautreporter.de' . $this->author->url);
 
-        $crawler->filter('header.article__header')->each(function (Crawler $node) {
-            $this->author->biography = trim($node->filter('.author__bio')->text());
+        if ($client->getResponse()->getStatus() == 200) {
+            $crawler->filter('header.article__header')->each(function (Crawler $node) {
+                $this->author->biography = trim($node->filter('.author__bio')->text());
 
-            if ($this->author->biography == '') {
-                $this->author->biography = null;
-            }
-
-            try {
-                $this->author->socialmedia = trim($node->filter('#author-page--media-links')->html());
-            } catch (\InvalidArgumentException $e) {
-            }
-
-            $this->author->save();
-
-            $imageUrls = $node->filter('h2.author--large img')->attr('srcset');
-            preg_match('/(.*) 170w, (.*) 340w/', $imageUrls, $matches);
-            if (count($matches) == 3) {
-                foreach ($matches as $index => $match) {
-                    if ($index == 0) {
-                        continue;
-                    }
-
-                    $image = Image::firstOrCreate([
-                        'imageable_id' => $this->author->id,
-                        'imageable_type' => 'App\Author',
-                        'width' => $index == 1 ? 170 : 340
-                    ]);
-
-                    $image->src = getenv('URL_KRAUTREPORTER') . $match;
-
-                    $this->author->images()->save($image);
+                if ($this->author->biography == '') {
+                    $this->author->biography = null;
                 }
-            }
 
-            $crawl = $this->author->crawl;
-            $crawl->next_crawl = Carbon::now()->addDay();
+                try {
+                    $this->author->socialmedia = trim($node->filter('#author-page--media-links')->html());
+                } catch (\InvalidArgumentException $e) {
+                }
 
-            $this->author->crawl()->save($crawl);
-        });
+                $this->author->save();
 
+                $imageUrls = $node->filter('h2.author--large img')->attr('srcset');
+                preg_match('/(.*) 170w, (.*) 340w/', $imageUrls, $matches);
+                if (count($matches) == 3) {
+                    foreach ($matches as $index => $match) {
+                        if ($index == 0) {
+                            continue;
+                        }
+
+                        $image = Image::firstOrCreate([
+                            'imageable_id' => $this->author->id,
+                            'imageable_type' => 'App\Author',
+                            'width' => $index == 1 ? 170 : 340
+                        ]);
+
+                        $image->src = getenv('URL_KRAUTREPORTER') . $match;
+
+                        $this->author->images()->save($image);
+                    }
+                }
+
+                $crawl = $this->author->crawl;
+                $crawl->next_crawl = Carbon::now()->addDay();
+
+                $this->author->crawl()->save($crawl);
+            });
+        }
     }
 }
