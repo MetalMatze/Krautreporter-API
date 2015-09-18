73a427182ff60e99be5384998f13d6fb2525fd50
Matthias Loibl <me@metalmatze.de>
2015-09-18 17:57:40 +0200

    Improve blade templates

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,12 @@
 <html>
-    <head>
-        <title>Krautreporter API</title>
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/paper/bootstrap.min.css"/>
-    </head>
-    <body>
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+<head>
+    <title>Krautreporter API</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+<script src="js/bundle.js"></script>
+</body>
 </html>
--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -1,6 +1,49 @@
 @extends('layouts.master')
 
 @section('content')
-    @include('crawls._table', ['crawls' => $nextAuthorCrawls])
-    @include('crawls._table', ['crawls' => $nextArticleCrawls])
+    <div id="status"></div>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Type</th>
+            <th>URL</th>
+            <th>Next Crawl</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Article</td>
+            <td>asdf</td>
+            <td>asdf</td>
+            <td>
+                <a href="" class="btn btn-xs btn-info">more</a>
+            </td>
+        </tr>
+        <tr>
+            <td>Author</td>
+            <td>asdf</td>
+            <td>asdf</td>
+            <td>
+                <a href="" class="btn btn-xs btn-info">more</a>
+            </td>
+        </tr>
+        <tr>
+            <td>asdf</td>
+            <td>asdf</td>
+            <td>asdf</td>
+            <td>
+                <a href="" class="btn btn-xs btn-info">more</a>
+            </td>
+        </tr>
+        <tr>
+            <td>asdf</td>
+            <td>asdf</td>
+            <td>asdf</td>
+            <td>
+                <a href="" class="btn btn-xs btn-info">more</a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
 @stop
