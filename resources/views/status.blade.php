9feab101f9dc2e7227b5d7dbf1b95003715f3855
Matthias Loibl <me@metalmatze.de>
2015-09-30 00:46:13 +0200

    Delete static html template in status.blade.php

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -1,49 +1,8 @@
 @extends('layouts.master')
 
 @section('content')
+    <h3>Status</h3>
+
+    <h4>Next crawls</h4>
     <div id="status"></div>
-    <table class="table table-hover">
-        <thead>
-        <tr>
-            <th>Type</th>
-            <th>URL</th>
-            <th>Next Crawl</th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Article</td>
-            <td>asdf</td>
-            <td>asdf</td>
-            <td>
-                <a href="" class="btn btn-xs btn-info">more</a>
-            </td>
-        </tr>
-        <tr>
-            <td>Author</td>
-            <td>asdf</td>
-            <td>asdf</td>
-            <td>
-                <a href="" class="btn btn-xs btn-info">more</a>
-            </td>
-        </tr>
-        <tr>
-            <td>asdf</td>
-            <td>asdf</td>
-            <td>asdf</td>
-            <td>
-                <a href="" class="btn btn-xs btn-info">more</a>
-            </td>
-        </tr>
-        <tr>
-            <td>asdf</td>
-            <td>asdf</td>
-            <td>asdf</td>
-            <td>
-                <a href="" class="btn btn-xs btn-info">more</a>
-            </td>
-        </tr>
-        </tbody>
-    </table>
 @stop
