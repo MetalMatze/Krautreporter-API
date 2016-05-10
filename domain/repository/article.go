dafc3416abd9c89eaf08b3bac8487374c7ab1d03
Matthias Loibl <mail@matthiasloibl.com>
2016-05-10 02:50:04 +0200

    Create article repository and improve author's

---
+++ domain/repository/article.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/MetalMatze/Krautreporter-API/domain/entity"
+	"log"
+	"strings"
+)
+
+var ErrArticleNotFound = errors.New("Article not found")
+
+type GormArticleRepository GormRepository
+
+func (r GormArticleRepository) FindByID(id int) (*entity.Article, error) {
+	var a entity.Article
+	r.DB.First(&a, "id = ?", id)
+
+	if a.ID == 0 {
+		return nil, ErrArticleNotFound
+	}
+
+	return &a, nil
+}
+
+func (r GormArticleRepository) SaveAll(articles []entity.Article) error {
+	tx := r.DB.Begin()
+	for i, a := range articles {
+		a.Ordering = len(articles) - 1 - i
+
+		var author entity.Author
+		r.DB.First(&author, "name = ?", strings.TrimSpace(a.Author.Name))
+		if author.ID == 0 {
+			log.Printf(`Can't find author "%s" for article %s`, a.Author.Name, a.URL)
+			continue
+		}
+		a.AuthorID = author.ID
+		a.Author = nil
+
+		if _, err := r.FindByID(a.ID); err == ErrArticleNotFound {
+			if result := tx.Create(&a); result.Error != nil {
+				return result.Error
+			}
+		} else {
+			if result := tx.Save(&a); result.Error != nil {
+				return result.Error
+			}
+		}
+	}
+	tx.Commit()
+
+	return nil
+}
--- domain/repository/author.go
+++ domain/repository/author.go
@@ -1,9 +1,13 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/MetalMatze/Krautreporter-API/domain/entity"
 	"github.com/jinzhu/gorm"
 )
+
+var ErrAuthorNotFound = errors.New("Author not found")
 
 type GormRepository struct {
 	DB *gorm.DB
@@ -11,29 +15,38 @@ type GormRepository struct {
 
 type GormAuthorRepository GormRepository
 
-func NewGormAuthorsRepository(db *gorm.DB) GormAuthorRepository {
-	return GormAuthorRepository{
-		DB: db,
-	}
-}
-
-func (r GormAuthorRepository) Find() ([]entity.Author, error) {
-	var authors []entity.Author
+func (r GormAuthorRepository) Find() ([]*entity.Author, error) {
+	var authors []*entity.Author
 	r.DB.Order("ordering desc").Find(&authors)
 
 	return authors, nil
 }
 
-func (r GormAuthorRepository) FindByID(id int) (entity.Author, error) {
+func (r GormAuthorRepository) FindByID(id int) (*entity.Author, error) {
 	var author entity.Author
 	r.DB.First(&author, "id = ?", id)
 
-	return author, nil
+	if author.ID == 0 {
+		return nil, ErrAuthorNotFound
+	}
+
+	return &author, nil
 }
 
 func (r GormAuthorRepository) SaveAll(authors []entity.Author) error {
-	for _, author := range authors {
-		r.DB.Create(&author)
+	tx := r.DB.Begin()
+	for _, a := range authors {
+		if _, err := r.FindByID(a.ID); err == ErrAuthorNotFound {
+			if result := tx.Create(&a); result.Error != nil {
+				return result.Error
+			}
+		} else {
+			if result := tx.Save(&a); result.Error != nil {
+				return result.Error
+			}
+		}
 	}
+	tx.Commit()
+
 	return nil
 }
