2604fc04762135e98cef12b49227ab94c63cfcc0
Matthias Loibl <mail@matthiasloibl.com>
2016-05-31 00:29:01 +0200

    Create crawler.go to create binary

---
+++ cmd/crawler/crawler.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/MetalMatze/Krautreporter-API/cli"
+	"github.com/MetalMatze/Krautreporter-API/config"
+	"github.com/MetalMatze/Krautreporter-API/domain"
+	"github.com/MetalMatze/gollection"
+	"github.com/MetalMatze/gollection/cache"
+	"github.com/MetalMatze/gollection/database"
+	"github.com/MetalMatze/gollection/router"
+)
+
+func main() {
+	config := config.GetConfig()
+	g := gollection.New(config)
+
+	g.AddDB(database.Postgres(config))
+	g.AddCache(cache.NewInMemory())
+	g.AddRouter(router.NewGin())
+
+	kr := domain.NewKrautreporter(g)
+
+	g.AddCommands(
+		cli.SyncCommand(kr),
+		cli.CrawlCommand(kr),
+	)
+
+	if err := g.Run(); err != nil {
+		g.Log.Crit("Error running gollection", "err", err)
+	}
+}
