669bbd635e46caba97d32f03a84bf1d35fd6400a
Matthias Loibl <mail@matthiasloibl.com>
2016-06-06 11:36:59 +0200

    Create marshaller.Article & Articles

---
+++ http/marshaller/article_marshaller.go
@@ -0,0 +1,68 @@
+package marshaller
+
+import (
+	"time"
+
+	"github.com/MetalMatze/Krautreporter-API/krautreporter/entity"
+)
+
+type articleMarshaller struct {
+	ID         int       `json:"id"`
+	Ordering   int       `json:"order"`
+	Title      string    `json:"title"`
+	Headline   string    `json:"headline"`
+	Date       string    `json:"date"`
+	Morgenpost bool      `json:"morgenpost"`
+	Preview    bool      `json:"preview"`
+	URL        string    `json:"url"`
+	Excerpt    string    `json:"excerpt"`
+	Content    string    `json:"content"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+
+	AuthorID int                          `json:"author_id"`
+	Images   map[string][]imageMarshaller `json:"images"`
+}
+
+func Article(a *entity.Article) map[string]articleMarshaller {
+	return map[string]articleMarshaller{
+		"data": marshallArticle(a),
+	}
+}
+
+func Articles(articles []*entity.Article) map[string][]articleMarshaller {
+	var as []articleMarshaller
+
+	for _, a := range articles {
+		as = append(as, marshallArticle(a))
+	}
+
+	return map[string][]articleMarshaller{
+		"data": as,
+	}
+}
+
+func marshallArticle(a *entity.Article) articleMarshaller {
+	am := articleMarshaller{
+		ID:         a.ID,
+		Ordering:   a.Ordering,
+		Title:      a.Title,
+		Headline:   a.Headline,
+		Date:       a.Date,
+		Morgenpost: false,
+		Preview:    a.Preview,
+		URL:        KrautreporterURL + a.URL,
+		Excerpt:    a.Excerpt,
+		Content:    a.Content,
+		CreatedAt:  a.CreatedAt,
+		UpdatedAt:  a.UpdatedAt,
+
+		AuthorID: a.AuthorID,
+	}
+
+	if len(a.Images) > 0 {
+		am.Images = Images(a.Images)
+	}
+
+	return am
+}
---
+++ http/marshaller/article_marshaller_test.go
@@ -0,0 +1,60 @@
+package marshaller
+
+import (
+	"testing"
+
+	"encoding/json"
+
+	"github.com/MetalMatze/Krautreporter-API/krautreporter/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshallArticle(t *testing.T) {
+	a := &entity.Article{
+		ID:       123,
+		Ordering: 10,
+		Title:    "Title",
+		Headline: "Headline",
+		Preview:  true,
+		URL:      "/123--article",
+		Excerpt:  "foo",
+		Content:  "bar",
+		AuthorID: 13,
+	}
+
+	b, err := json.Marshal(Article(a))
+	assert.Nil(t, err)
+	assert.JSONEq(
+		t,
+		`{"data":{"id":123,"order":10,"title":"Title","headline":"Headline","date":"","morgenpost":false,"preview":true,"url":"https://krautreporter.de/123--article","excerpt":"foo","content":"bar","created_at":"0001-01-01T00:00:00Z","updated_at":"0001-01-01T00:00:00Z","author_id":13,"images":null}}`,
+		string(b),
+	)
+
+	a.Images = append(a.Images, entity.Image{ID: 123, Width: 256, Src: "/foo.jpg"})
+
+	b, err = json.Marshal(Article(a))
+	assert.Nil(t, err)
+	assert.JSONEq(
+		t,
+		`{"data":{"id":123,"order":10,"title":"Title","headline":"Headline","date":"","morgenpost":false,"preview":true,"url":"https://krautreporter.de/123--article","excerpt":"foo","content":"bar","created_at":"0001-01-01T00:00:00Z","updated_at":"0001-01-01T00:00:00Z","author_id":13,"images":{"data":[{"id":123,"width":256,"src":"https://krautreporter.de/foo.jpg"}]}}}`,
+		string(b),
+	)
+}
+
+func TestMarshallArticles(t *testing.T) {
+	articles := []*entity.Article{{
+		ID:       1,
+		Ordering: 1,
+	}, {
+		ID:       2,
+		Ordering: 0,
+	}}
+
+	b, err := json.Marshal(Articles(articles))
+	assert.Nil(t, err)
+	assert.JSONEq(
+		t,
+		`{"data":[{"id":1,"order":1,"title":"","headline":"","date":"","morgenpost":false,"preview":false,"url":"https://krautreporter.de","excerpt":"","content":"","created_at":"0001-01-01T00:00:00Z","updated_at":"0001-01-01T00:00:00Z","author_id":0,"images":null},{"id":2,"order":0,"title":"","headline":"","date":"","morgenpost":false,"preview":false,"url":"https://krautreporter.de","excerpt":"","content":"","created_at":"0001-01-01T00:00:00Z","updated_at":"0001-01-01T00:00:00Z","author_id":0,"images":null}]}`,
+		string(b),
+	)
+}
