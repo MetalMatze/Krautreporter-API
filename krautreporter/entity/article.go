7eabc3112a51b091d8aef5ef0794405b1326328f
Matthias Loibl <mail@matthiasloibl.com>
2016-06-06 11:37:46 +0200

    Remove json from article entity

--- krautreporter/entity/article.go
+++ krautreporter/entity/article.go
@@ -3,20 +3,20 @@ package entity
 import "time"
 
 type Article struct {
-	ID        int       `json:"id"`
-	Ordering  int       `json:"order"`
-	Title     string    `json:"title"`
-	Headline  string    `json:"headline"`
-	Date      string    `json:"date"`
-	Preview   bool      `json:"preview"`
-	URL       string    `json:"url"`
-	Excerpt   string    `json:"excerpt"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int
+	Ordering  int
+	Title     string
+	Headline  string
+	Date      string
+	Preview   bool
+	URL       string
+	Excerpt   string
+	Content   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
-	AuthorID int     `json:"author_id"`
-	Author   *Author `json:"-"`
-	Images   []Image `gorm:"polymorphic:Imageable;" json:"-"`
-	Crawl    Crawl   `gorm:"polymorphic:Crawlable;" json:"-"`
+	AuthorID int
+	Author   *Author
+	Images   []Image `gorm:"polymorphic:Imageable;"`
+	Crawl    Crawl   `gorm:"polymorphic:Crawlable;"`
 }
