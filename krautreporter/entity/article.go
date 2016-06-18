f2b2fddbeb3aec1a4aeff0b4ece76bfb2f0a7702
Matthias Loibl <mail@matthiasloibl.com>
2016-06-18 15:08:32 +0200

    Create AddImage for Article

--- krautreporter/entity/article.go
+++ krautreporter/entity/article.go
@@ -20,3 +20,15 @@ type Article struct {
 	Images   []Image `gorm:"polymorphic:Imageable;"`
 	Crawl    Crawl   `gorm:"polymorphic:Crawlable;"`
 }
+
+// AddImage adds an image to the article and makes sure that there's only one image for each width
+func (a *Article) AddImage(i Image) {
+	for key, image := range a.Images {
+		if image.Width == i.Width {
+			a.Images[key].Src = i.Src
+			return
+		}
+	}
+
+	a.Images = append(a.Images, i)
+}
