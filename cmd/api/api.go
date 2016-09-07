7dac81a15ae9ea12334319df863cee23245e13e3
Matthias Loibl <mail@matthiasloibl.com>
2016-09-07 02:27:43 +0200

    Delete bootstrap.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	nethttp "net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-kit/kit/log"
 	"github.com/gollection/gollection"
-	"github.com/metalmatze/krautreporter-api/cmd"
+	"github.com/gollection/gollection/database/gorm/postgres"
+	gogin "github.com/gollection/gollection/router/gin"
+	"github.com/jinzhu/gorm"
 	"github.com/metalmatze/krautreporter-api/config"
 	"github.com/metalmatze/krautreporter-api/http"
 	"github.com/metalmatze/krautreporter-api/krautreporter"
+	gocache "github.com/patrickmn/go-cache"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -21,9 +26,9 @@ func main() {
 	c := config.Config()
 	g := gollection.New(logger, c.Config)
 
-	router := cmd.Gin(g, c)
-	gorm := cmd.Gorm(g, c)
-	cache := cmd.Cache()
+	router := Gin(g, c)
+	gorm := Gorm(g, c)
+	cache := Cache()
 
 	kr := krautreporter.New(logger, gorm, cache)
 
@@ -48,4 +53,25 @@ func main() {
 	if err := g.Run(); err != nil {
 		g.Logger.Log("msg", "Error running gollection")
 	}
+}
+
+func Gorm(g *gollection.Gollection, c config.AppConfig) *gorm.DB {
+	gorm, err := postgres.New(g.Logger, c.DatabaseConfig)
+	if err != nil {
+		fmt.Printf("%+v\n", c.DatabaseConfig)
+		panic(err)
+	}
+
+	return gorm
+}
+
+func Gin(g *gollection.Gollection, c config.AppConfig) *gin.Engine {
+	ginWrapper := gogin.New(g.Logger, c.RouterConfig)
+	g.Register(ginWrapper)
+
+	return ginWrapper.Engine
+}
+
+func Cache() *gocache.Cache {
+	return gocache.New(5*time.Minute, 30*time.Second)
 }
